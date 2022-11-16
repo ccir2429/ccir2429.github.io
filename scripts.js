feb501aaffcb1de5caa352e47b3cbda48ea8cf4a
Cosmin <cosma.cosmin97@gmail.com>
2022-11-16 14:55:20 +0200

    play again?

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// document.addEventListener('DOMContentLoaded',function(){
+//     //d-w--r---r--    
+//     let linkElements = this.getElementsByClassName('linkHolder')
+//     linkElements.array.forEach(element => {
+//         let newLink = 'd-w--r---r--'+element.firstElementChild
+//         if (element.hasChildNodes()) {
+//             element.replaceChild(element.firstElementChild,newLink);
+//           }
+//     });
+// },false);
